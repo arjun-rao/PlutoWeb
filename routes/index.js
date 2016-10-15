eaeaf3cd3ed87c44043f6775d5fdd0416da20635
arjun-rao <mailarjunrao@gmail.com>
2016-10-15 06:25:04 +0530

    Fixed Bugs

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ router.post('/analyze', function(req, res, next) {
 			entry.save(function(err) {
 				if (err) {
 					console.log(err);
-					res.status(500).json({status:"db save failed",user:req_user_id});
+					res.status(500).json({status:"db save failed"});
 					throw err;
 				}
 				console.log('Entry saved successfully!');
@@ -64,64 +64,21 @@ router.post('/analyze', function(req, res, next) {
 });
 
 router.post('/post', function(req,res,next) {
+
 	var req_user_id = req.body.user_id;
 	var req_text = req.body.payload;
 	var req_day = req.body.day;
 	var req_month = req.body.month;
 	var req_year = req.body.year;
-
-	console.log(req.body);
+	
 
 	// if our user.js file is at app/models/user.js
 	var diaryEntry = require('../models/diaryEntry.js');
 	
-	/*diaryEntry
-	.findone({
-			user_id:req_user_id,
-			'date.day': req_day,
-			'date.month': req_month,
-			'date.year': req_year
-	})
-	.exec(function(err, existing_entry) {
-		if ( err ) {
-			console.log(err);
-			throw err;
-		}
-		//existing entry for that day is found, stop registration
-		if ( existing_entry ) {
-			console.log("entry already exists");
-			return;
-		}
-		console.log("entry created");
-	});*/
-
-	var entry = new diaryEntry({
-			user_id: req_user_id,
-			date: {
-				day: req_day,
-				month: req_month,
-				year: req_year
-			},
-			body: req_text
-		});
-
-		entry.save(function(err) {
-  		if (err) {
-  			console.log(err);
-  			res.status(500).json({status:"failed"});
-  			return;
-  		}
-		console.log('Entry saved successfully!');
-		res.status(200).json({status:"ok",user:req_user_id,date:''+req_day+'-'+req_month+'-'+req_year});
-	});
-	var text = req.body.payload;
-	var time = Date.now();
+	
 	
 	//Watson Code Starts Here
-	var watson_reply = '';
-	var req_user_id = req.body.user_id;
-	var req_text = req.body.payload;
-	var time = Date.now();
+	var watson_reply = '';	
 	var parameters = {
 		extract: 'entities,doc-emotion',
 		sentiment: 1,
@@ -134,29 +91,33 @@ router.post('/post', function(req,res,next) {
 		if (err)
 		{
 			console.log("Watson Died!");
-			return next(err);
+			res.status(500).json({status:"Watson API failed"});
+			return; 
 		}
 		else
 		{
 			console.log(JSON.stringify(response, null, 2));
-			watson_reply = JSON.stringify(response,null,2);
-			//save everything to the local db														
-			var diaryEntry = require('../models/diaryEntry.js');
+			watson_reply = JSON.stringify(response,null,2);																				
 			// create a new user called chris
 			var entry = new diaryEntry({
-			user_id: req_user_id,
-			body: req_text,
-			watson_response: watson_reply 
-			});
+				user_id: req_user_id,
+				body: req_text,
+				date: {
+					day: req_day,
+					month: req_month,
+					year: req_year
+				},
+				watson_response: watson_reply 
+				});
 
 			entry.save(function(err) {
 				if (err) {
 					console.log(err);
-					res.status(500).json({status:"db save failed",user:req_user_id});
-					throw err;
+					res.status(500).json({status:"db save failed"});
+					return;
 				}
 				console.log('Entry saved successfully!');
-				res.status(200).json({status:"ok",user:req_user_id});
+				res.status(200).json({status:"ok"});
 			});
 		}
 	});	
