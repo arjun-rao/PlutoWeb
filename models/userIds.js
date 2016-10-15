ea592da9a3795c3191953a015efc4025bf5cdd44
Abhilash K <abhilash1in@gmail.com>
2016-10-15 12:31:37 +0530

    abhilash changes

---
+++ models/userIds.js
@@ -0,0 +1,17 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var userIdSchema = new Schema({
+  user_id: { type: String, required: true, unique: true},
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var userIdEntry = mongoose.model('UserIdEntry', userIdSchema);
+
+// make this available to our users in our Node applications
+module.exports = userIdEntry;
